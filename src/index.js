3b296650324a47a6806873f5663a3d7ca90b261d
unknown <ayoubnasraoui161@yahoo.fr>
2023-07-29 18:21:22 +0200

    Create a reusible component Star rating

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles.css"
-import App from "./components/App"
+//import "./styles.css"
+//import App from "./components/App"
+import StarRating from "./components/StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-     <App/>
+        {/*<App/> */}
+        <StarRating
+            maxRating={5}
+            messages={["terrible", "bad", "okay", "good", "amazing"]}
+        />
+        <StarRating size={24} color="red" className="test" defaultRating={3} />
     </React.StrictMode>
 );
